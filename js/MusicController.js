4b2789d126746a23b25043b5c1f6d7e096ec2de9
johnmryan <johnmryan830@gmail.com>
2016-03-21 19:04:18 -0400

    Added radio buttons

--- js/MusicController.js
+++ js/MusicController.js
@@ -2,10 +2,11 @@ var app = angular.module('musicApp',[]);
 app.controller('MusicController', ['$scope', function($scope) {
 	var vm = this;
 	vm.notes = undefined;
+	vm.order = "first";
 	
 	vm.generate = function() {
 		if (vm.notes) {
-			console.log("vm.notes is defined");
+			console.log(vm.order);
 		}
 	}
 
