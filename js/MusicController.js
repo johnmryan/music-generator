4ea851f7d769571af5762146b8f388ffaa192515
johnmryan <johnmryan830@gmail.com>
2016-03-21 23:15:16 -0400

    gh pages

---
+++ js/MusicController.js
@@ -0,0 +1,154 @@
+var app = angular.module('musicApp',[]);
+app.controller('MusicController', ['$scope', function($scope) {
+	var vm = this;
+	vm.notes = undefined;
+	vm.order = "first";
+	vm.generated_songs = undefined;
+	var frequencies = new Array();
+	frequencies['C'] = 261.63;
+	frequencies['D'] = 293.66;
+	frequencies['E'] = 329.63;
+	frequencies['F'] = 349.23;
+	frequencies['G'] = 392.00;
+	frequencies['A'] = 440.00;
+	frequencies['B'] = 493.88;
+
+	vm.generate = function() {
+		if (vm.notes) {
+			var music = vm.notes;
+			if (vm.order === "first") {
+				var markov_dict = build_markov_dict(music);
+				var scores = [];
+				for (var i=0;i<10;i++) {
+					var score = generate_score(markov_dict);
+					scores.push(score);
+				}
+				vm.generated_songs = scores;
+			}
+			else {
+				var markov_dict = build_second_order(music);
+				var scores = [];
+				for (var i=0;i<10;i++) {
+					var score = generate_second_order(markov_dict);
+					scores.push(score);
+				}
+				scores.pop();
+				vm.generated_songs = scores;
+			}
+		}
+	}
+
+	vm.playSong = function($parentIndex) {
+		var song = $parentIndex;
+		var timber_objects = createTimberObjects(song);
+		playNote(0, timber_objects);
+	}
+
+	//Helper functions	
+	function playNote(p, objs){
+		if (p < objs.length){
+			T("perc", {r:250}, objs[p]).on("ended", function() {
+				this.pause();
+				playNote(p+1, objs); // Recursive call to play a note until song is over
+			}).bang().play();
+		}
+	}
+
+	function createTimberObjects(song) {
+		var objs = [];
+		for (var i=0; i<song.length;i++) {
+			objs.push(T("saw", {freq:frequencies[song[i].toUpperCase()]}));
+		}
+		return objs;
+	}
+
+	function random_choice(choices) {
+		var index = Math.floor(Math.random() * choices.length);
+		return choices[index];
+	}
+
+	function build_markov_dict(s) {
+		// Builds a dictionary using input string
+		var markov_dict = new Array();
+		for (var i=0; i < s.length; i++) {
+			if (!(s[i] in markov_dict)) {
+				markov_dict[s[i]] = [s[i+1]];
+			}
+			else {
+				markov_dict[s[i]].push(s[i+1]);
+			}
+		}
+		return markov_dict;
+	}
+   function build_second_order(s){
+		var aMap = {};
+		for (var i=0; i < s.length-2; i++) {
+			key = s[i]+s[i+1];
+			value=s[i+2];
+			aMap[key] = aMap[key] || [];
+			aMap[key].push(value);
+		}
+		return aMap;
+   }
+	function generate_second_order(markov_dict){	
+		var most_freq = Object.keys(markov_dict)[0];
+		var len = markov_dict[most_freq].length;
+		for (key in markov_dict) {
+			if (markov_dict[key].length > len) {
+				most_freq = key;
+				len = markov_dict[key].length;
+			}
+		}
+		var current = most_freq;
+		var score = [current[0], current[1]];
+		var i = 0;
+		while (current in markov_dict && i < 30) {
+			c = random_choice(markov_dict[current]);
+			score.push(c);
+			current = score[score.length-2]+score[score.length-1];
+			i +=2;
+		}
+		return score;
+	}
+						   
+	function build_charset(notes) {
+		char_set = {};
+		for (var i=0; i < notes.length; i++) {
+			if (!(notes[i] in char_set)) {
+				char_set[notes[i]] = true;
+			}
+		}
+		var keys = [];
+		for (k in char_set) {
+			if (char_set.hasOwnProperty(k)) {
+				keys.push(k);
+			}
+		}
+		keys.sort();
+		return keys;
+	}
+
+	function generate_score(markov_dict) {
+		//return most frequent
+		var most_freq = Object.keys(markov_dict)[0];
+		var len = markov_dict[most_freq].length;
+		for (key in markov_dict) {
+			if (markov_dict[key].length > len) {
+				most_freq = key;
+				len = markov_dict[key].length;
+			}
+		}
+		var current = most_freq;
+		var score = [current];
+		var i = 0;
+		while (current in markov_dict && i < 30) {
+			c = random_choice(markov_dict[current]);
+			score.push(c);
+			current = c;
+			i++;
+		}
+		score.pop();
+		return score;
+	}
+	
+}]);
---
+++ python/music_generator.py
@@ -0,0 +1,120 @@
+# Jack Ryan
+# 3/20/2016
+# CSE 30151
+# music_generator.py
+# Goal: support at least first and second order music generation
+
+import random
+
+def build_markov_dict(s):
+	# Builds up a dictionary using input string
+	# e.g. {A -> [A, A, B, C, A], B -> [A, B, A, C, C], ...}
+	markov_dict = {}
+	for i in xrange(0, len(s)-1):
+		if not s[i] in markov_dict:
+			# If the character is not a key yet, initialize value to list with next char
+			markov_dict[s[i]] = [s[i+1]]
+		else:
+			markov_dict[s[i]].append(s[i+1])
+	return markov_dict
+
+def build_charset(notes):
+	char_set = set()
+	for char in notes:
+		char_set.add(char)
+	sorted_set = sorted(char_set)
+	return sorted_set
+def build_fo_adjlist(notes):
+	# Builds a first order Markov chain
+	markov_dict = build_markov_dict(notes)
+	sorted_set = build_charset(notes)
+	adjlist = [None] * len(sorted_set)
+	i = 0
+	for char in sorted_set:
+		if char not in markov_dict:
+			adjlist[i] = []
+		else:
+			adjlist[i] = markov_dict[char]
+		i += 1
+	return adjlist
+
+#TODO
+def build_second_order(notes):
+	m_dict = {}
+	for i in xrange(0, len(notes)-2):
+		current_pair=(notes[i]+notes[i+1])
+		m_dict.setdefault(current_pair,[]).append(notes[i+2])
+	return m_dict
+
+
+def generate_score(markov_dict):
+	most_freq = max(markov_dict, key= lambda x: len(markov_dict[x]))
+	current = most_freq
+	score = [current]
+	i = 0
+	while (current in markov_dict) and i < 100:
+		char = random.choice(markov_dict[current])
+		score.append(char)
+		current = char
+		i += 1
+	return " ".join(score)
+
+def generate_score_second_order(markov_dict):
+	most_freq = max(markov_dict, key= lambda x: len(markov_dict[x]))
+	current = most_freq
+	score = [current[0], current[1]]#contains the first an second elements
+	i = 0
+	while (current in markov_dict) and i < 100:
+		char = random.choice(markov_dict[current])
+		score.append(char)
+		current = score[len(score)-2]+score[len(score)-1] #current is the last two elements in in the list
+		i += 2 
+	return " ".join(score)
+
+
+## Main ##
+
+frequency_map = {'C': 261.63,'D': 293.66,'E': 329.63,'F':349.23,'G':392.00,'A':440.00,'B':493.88}
+# Using middle C octave for simplicity, can easily be extended to support 'C4', 'E2', etc
+
+print "Enter the string of notes to be used in this Markov music generator:",
+notes = raw_input()
+
+adj_list = build_fo_adjlist(notes)
+charset = build_charset(notes)
+
+
+print "\nFirst-order adjacency list representing transitions on current musical note\n"
+for i in xrange(0, len(adj_list)):
+	print charset[i] + ": " + str(adj_list[i])
+
+print "\nGenerating 20 musical scores based on Markov first-order music generation model...\n"
+
+markov_dict = build_markov_dict(notes)
+
+score_list = []
+
+for i in xrange(0, 20):
+	score = generate_score(markov_dict)
+	score_list.append(score)
+	print str(i) + ": " + score + "\n"
+
+
+#SECOND ORDER ----
+print "\nsecond-order adjacency list representing transitions on current musical note\n"
+
+s= build_second_order(notes)
+for key, value in s.iteritems():
+	print "key: "+key + " values: "+ str(value)
+
+score_list = []
+
+for i in xrange(0, 20):
+	score = generate_score_second_order(s)
+	score_list.append(score)
+	print str(i) + ": " + score + "\n"
+
+
+
+
+
